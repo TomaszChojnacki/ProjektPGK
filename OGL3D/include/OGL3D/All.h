4a27732733c3cbd9bd5d3c49d8bcf20c07aad7ce
TomaszChojnacki <148254085+TomaszChojnacki@users.noreply.github.com>
2024-01-01 01:44:31 +0100

    Delete OGL3D directory

--- OGL3D/include/OGL3D/All.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-/// Includujemy naglowki z biblioteki OGL3D zwiazane z encjami (Entity).
-#include <OGL3D/Entity/OEntity.h>
-#include <OGL3D/Entity/OEntitySystem.h>
-
-/// Includujemy naglowek z biblioteki OGL3D zwiazanego z glownym obiektem silnika (Game).
-#include <OGL3D/Game/OGame.h>
